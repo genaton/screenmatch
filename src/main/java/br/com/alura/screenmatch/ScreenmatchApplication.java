edb03869b761a9bf6feeea5e8934ae8a15b003eb
genaton <genaton@gmail.com>
2025-05-20 20:09:15 -0300

    implementa várias refatorações; cria as classes DadosEpisodio, DadosTemporada e Principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
-import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,17 +15,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override	
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		// var json = consumoApi.obterDados("https://api.themoviedb.org/3/tv/popular?api_key=9fbc83be52fd2c56205d3433825fdde6");
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=9785c32c");
-		System.out.println("os filmes podulares são:  " + json);
+		
+		
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
-		// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		// System.out.println("imagem no seguinte link:  " + json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-		System.out.println(dados);
+	
 	}
 
 }
